7c93dff68126b91f383bd220125eb51cb4a52d6f
Selvakumar Boominathan <selvans2k@gmail.com>
2026-09-02 00:41:55 +0530

    Macro added in btc_utils- convert_to_usd

---
+++ macros/btc_utils.sql
@@ -0,0 +1,11 @@
+{% macro convert_to_usd(column_name) %}
+
+  {{ column_name}} * (
+    SELECT 
+    CLOSE_PRICE_USD
+    FROM {{ ref('btc_usd_max')}}
+    WHERE to_TIMESTAMP(replace(event_date, 'UTC','')) = current_timestamp()
+  )
+
+
+{% endmacro %}
--- models/marts/whale_alert.sql
+++ models/marts/whale_alert.sql
@@ -12,19 +12,12 @@ WHERE output_value > 10
 GROUP BY output_address
 ORDER BY total_sent DESC
 
-),
-LATEST_PRICE AS (
-  SELECT 
-  CLOSE_PRICE_USD
-  FROM {{ ref('btc_usd_max')}}
-  WHERE to_TIMESTAMP(replace(event_date, 'UTC','')) = current_timestamp()
 )
 
 SELECT 
   w.output_address,
   w.total_sent,
   w.tx_count,
-  (p.CLOSE_PRICE_USD * w.total_sent) AS price
+  {{ convert_to_usd('w.total_sent') }} AS price
 FROM WHALES w
-CROSS JOIN LATEST_PRICE p
 ORDER BY total_sent desc
