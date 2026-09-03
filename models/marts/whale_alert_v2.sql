d7ca74fd80a54ad0855df8a68533d2a20aab5c94
Selvakumar Boominathan <selvans2k@gmail.com>
2026-09-03 20:00:14 +0530

    created versioned whale_alert models

---
+++ models/marts/whale_alert_v2.sql
@@ -0,0 +1,22 @@
+{{ config(materialized='table') }}
+
+
+WITH WHALES AS (
+SELECT
+output_address,
+SUM(output_value) AS total_sent,
+COUNT(*) AS tx_count
+FROM {{ ref('stg_btc_transactions')}}
+
+WHERE output_value > 10
+GROUP BY output_address
+ORDER BY total_sent DESC
+
+)
+
+SELECT 
+  w.output_address,
+  w.total_sent,
+  w.tx_count
+FROM WHALES w
+ORDER BY total_sent desc
